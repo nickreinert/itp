7000437f7846a322d54446cf02261dde2aa50bf6
Nickolas Reinert <nickolasreinert@gmail.com>
2025-02-25 19:46:35 -0500

    fixed mixup with js file and formatting of readme

---
+++ FtoC Assignment/FtoC.js
@@ -0,0 +1,7 @@
+const fahrenheit = prompt("Enter Temperature in Fahrenheit");
+
+(parseInt(fahrenheit), typeof parseInt(fahrenheit));
+
+const celsius = ((fahrenheit - 32) * (5 / 9));
+
+console.log (celsius);
