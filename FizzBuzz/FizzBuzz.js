12c532f64c49008879e9cddd580839ed0061a784
Nickolas Reinert <nickolasreinert@gmail.com>
2025-03-05 23:48:17 -0500

    Completed FizzBuzz Assignment

---
+++ FizzBuzz/FizzBuzz.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 100; i++) {
+
+  if (i % 3 === 0 && i % 5 === 0) {console.log("FizzBuzz")}
+  if (i % 3 === 0) {console.log("Fizz")}
+  if (i % 5 === 0) {console.log("Buzz")}
+  else {console.log(i)}
+    
+
+}
